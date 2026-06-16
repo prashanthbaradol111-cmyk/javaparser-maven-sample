7e673055c071c4b95141878cec241d71f1e56739
prashanthbaradol111-cmyk <prashanthbaradol111@gmail.com>
2026-06-16 07:54:36 +0530

    Create Calculator.java

---
+++ src/main/java/com/yourorganization/maven_sample/Calculator.java
@@ -0,0 +1,15 @@
+package com.yourorganization.maven_sample;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
+
+//added this file 
